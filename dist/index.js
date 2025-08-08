f94bdc3259bd74ea1645dec1814b1b5b724c14ce
Ayan Roy <154493815+Ayanroy004@users.noreply.github.com>
2025-08-09 00:04:52 +0530

    scale on socket using redis multiple server handel

--- dist/index.js
+++ dist/index.js
@@ -7,21 +7,79 @@ const express_1 = __importDefault(require("express"));
 const socket_io_1 = require("socket.io");
 const http_1 = require("http");
 const path_1 = __importDefault(require("path"));
+const ioredis_1 = __importDefault(require("ioredis"));
 const app = (0, express_1.default)();
 const httpServer = (0, http_1.createServer)(app);
 const io = new socket_io_1.Server(httpServer);
 const PORT = process.env.PORT || 8000;
-let checkbox = new Array(100).fill(false);
-io.on("connection", (socket) => {
+const redis = new ioredis_1.default({ host: "localhost", port: 6379 });
+const publisher = new ioredis_1.default({ host: "localhost", port: 6379 });
+const subscriber = new ioredis_1.default({ host: "localhost", port: 6379 });
+let checkbox = new Array(10).fill(false);
+// redis.set("checkbox", JSON.stringify(checkbox)); // Update Redis with new state
+subscriber.subscribe("server:broker");
+subscriber.on("message", async (channel, message) => {
+    if (message !== "") {
+        const { checked, index } = JSON.parse(message);
+        const updatedCheckbox = await redis.get("checkbox");
+        const TotalTic = await redis.get("totalCheckboxTic");
+        console.log("Count : ", TotalTic);
+        if (updatedCheckbox) {
+            const final = await JSON.parse(updatedCheckbox);
+            console.log("updated checkbox : ", final);
+        }
+        const count = Number(TotalTic);
+        io.emit("checkboxUpdate", { index, checked, count });
+    }
+    else {
+        const flag = 1;
+        io.emit("checkboxUpdate", { checkbox, flag });
+    }
+});
+io.on("connection", async (socket) => {
     console.log("User Connected:", socket.id);
-    socket.on("checkboxChange", (data) => {
+    socket.on("checkboxChange", async (data) => {
         const { index, checked } = data;
-        checkbox[index] = checked;
-        io.emit("checkboxUpdate", { index, checked });
+        const cachedCheckbox = await redis.get("checkbox");
+        if (cachedCheckbox) {
+            try {
+                const jsonCached = JSON.parse(cachedCheckbox);
+                jsonCached[index] = checked;
+                const totalTic = await redis.get("totalCheckboxTic");
+                console.log("update tic", totalTic);
+                await redis.incr("totalCheckboxTic");
+                await redis.set("checkbox", JSON.stringify(jsonCached));
+            }
+            catch (e) {
+                console.log("Err : ", e);
+            }
+        }
+        await publisher.publish("server:broker", JSON.stringify({ checked, index }));
+        // io.emit("checkboxUpdate", { index, checked });
+    });
+    socket.on("clear", async () => {
+        await redis.del("checkbox");
+        await redis.del("totalCheckboxTic");
+        publisher.publish("server:broker", "");
     });
 });
-app.get("/state", (req, res) => {
-    res.json(checkbox);
+app.get("/state", async (req, res) => {
+    const cachedCheckbox = await redis.get("checkbox");
+    const totalTic = await redis.get("totalCheckboxTic");
+    if (cachedCheckbox) {
+        try {
+            const cachedParseData = await JSON.parse(cachedCheckbox);
+            console.log("Mil gaya", totalTic);
+            return res.json({ cachedParseData, totalTic });
+        }
+        catch (error) {
+            console.error("Error parsing checkbox state from Redis:", error);
+            return res.status(500).json({ error: "Internal Server Error" });
+        }
+    }
+    await redis.set("checkbox", JSON.stringify(checkbox));
+    await redis.set("totalCheckboxTic", "0");
+    return res.json(checkbox);
 });
 // ✅ Just use __dirname directly — it's built-in
 app.use(express_1.default.static(path_1.default.join(__dirname, "../public")));
--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,96 @@ import express, { Request, Response } from "express";
 import { Server, Socket } from "socket.io";
 import { createServer } from "http";
 import path from "path";
-
+import Redis from "ioredis";
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 const PORT = process.env.PORT || 8000;
 
-let checkbox = new Array(100).fill(false);
+const redis = new Redis({ host: "localhost", port: 6379 });
+const publisher = new Redis({ host: "localhost", port: 6379 });
+const subscriber = new Redis({ host: "localhost", port: 6379 });
 
-io.on("connection", (socket: Socket) => {
+let checkbox = new Array(10).fill(false);
+// redis.set("checkbox", JSON.stringify(checkbox)); // Update Redis with new state
+
+subscriber.subscribe("server:broker");
+subscriber.on("message", async (channel, message) => {
+  if (message !== "") {
+    const { checked, index } = JSON.parse(message);
+    const updatedCheckbox = await redis.get("checkbox");
+    const TotalTic = await redis.get("totalCheckboxTic");
+    console.log("Count : ", TotalTic);
+
+    if (updatedCheckbox) {
+      const final = await JSON.parse(updatedCheckbox);
+      console.log("updated checkbox : ", final);
+    }
+    const count = Number(TotalTic);
+    io.emit("checkboxUpdate", { index, checked, count });
+  } else {
+    const flag = 1;
+    io.emit("checkboxUpdate", { checkbox, flag });
+  }
+});
+
+io.on("connection", async (socket: Socket) => {
   console.log("User Connected:", socket.id);
 
-  socket.on("checkboxChange", (data: { index: number; checked: boolean }) => {
-    const { index, checked } = data;
-    checkbox[index] = checked;
-    io.emit("checkboxUpdate", { index, checked });
+  socket.on(
+    "checkboxChange",
+    async (data: { index: number; checked: boolean }) => {
+      const { index, checked } = data;
+      const cachedCheckbox = await redis.get("checkbox");
+      if (cachedCheckbox) {
+        try {
+          const jsonCached = JSON.parse(cachedCheckbox);
+          jsonCached[index] = checked;
+
+          const totalTic = await redis.get("totalCheckboxTic");
+          console.log("update tic", totalTic);
+
+          await redis.incr("totalCheckboxTic");
+
+          await redis.set("checkbox", JSON.stringify(jsonCached));
+        } catch (e) {
+          console.log("Err : ", e);
+        }
+      }
+      await publisher.publish(
+        "server:broker",
+        JSON.stringify({ checked, index })
+      );
+
+      // io.emit("checkboxUpdate", { index, checked });
+    }
+  );
+
+  socket.on("clear", async () => {
+    await redis.del("checkbox");
+    await redis.del("totalCheckboxTic");
+
+    publisher.publish("server:broker", "");
   });
 });
 
-app.get("/state", (req: Request, res: Response) => {
-  res.json(checkbox);
+app.get("/state", async (req: Request, res: Response) => {
+  const cachedCheckbox = await redis.get("checkbox");
+  const totalTic = await redis.get("totalCheckboxTic");
+  if (cachedCheckbox) {
+    try {
+      const cachedParseData = await JSON.parse(cachedCheckbox);
+
+      console.log("Mil gaya", totalTic);
+      return res.json({ cachedParseData, totalTic });
+    } catch (error) {
+      console.error("Error parsing checkbox state from Redis:", error);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+  await redis.set("checkbox", JSON.stringify(checkbox));
+  await redis.set("totalCheckboxTic", "0");
+  return res.json(checkbox);
 });
 
 // ✅ Just use __dirname directly — it's built-in
